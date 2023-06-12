b296c6b9e66394654fa392125ec9872b7fee5ee6
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-06-12 15:28:03 -0400

    feat: create LoginScreen

---
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+import {Screen} from '../../../components/Screen/Screen';
+import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
+
+export function LoginScreen() {
+  return (
+    <Screen>
+      <Text preset="headingLarge" mb="s8">
+        Olá!
+      </Text>
+      <Text preset="paragraphLarge" mb="s40">
+        Digite seu e-mail e senha para entrar
+      </Text>
+
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
+
+      <PasswordInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's10'}}
+      />
+
+      <Text preset="paragraphSmall" bold color="primary">
+        Esqueci minha senha
+      </Text>
+
+      <Button title="Entrar" mt="s48" />
+      <Button preset="outline" title="Criar uma Conta" mt="s12" />
+    </Screen>
+  );
+}
