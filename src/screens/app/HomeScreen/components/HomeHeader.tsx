6e895a2f4851099255a426c8ec0bc3ce81155243
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-06-28 13:27:13 -0400

    feat: add SimpleLogo to project

--- src/screens/app/HomeScreen/components/HomeHeader.tsx
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {SimpleLogo} from '@brand';
+
 import {Box, BoxProps, Icon} from '@components';
 import {useAppSafeArea} from '@hooks';
 
@@ -11,7 +13,7 @@ export function HomeHeader() {
       style={{
         paddingTop: top,
       }}>
-      <Box />
+      <SimpleLogo width={70} />
       <Box flexDirection="row">
         <Box mr="s24">
           <Icon name="search" />
