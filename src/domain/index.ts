d906b6faa6f8ade79f944a3960f5a5c6f504123d
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-07-29 19:27:24 -0400

    feat: post comment api, service and adapter

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,6 @@
 export * from './Post/postService';
 export * from './Post/postTypes';
+export * from './PostComment/postCommentService';
+export * from './PostComment/postCommentTypes';
 
 export * from './Post/useCases/usePostList';
