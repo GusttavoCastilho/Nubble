476135bf089ce124cb1cd1b8adfcdf22375f1278
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-07-30 19:15:53 -0400

    feat: usePaginatedList Post and PostComment

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,3 +4,6 @@ export * from './PostComment/postCommentService';
 export * from './PostComment/postCommentTypes';
 
 export * from './Post/useCases/usePostList';
+export * from './PostComment/useCases/usePostCommentList';
+
+export * from './hooks/usePaginatedList';
