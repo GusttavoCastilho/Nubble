da4c1e9217b17c403294409dceaebc2706cae603
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-08-21 10:43:32 -0400

    feat: add react query

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -1,32 +1,16 @@
-import {useCallback, useEffect, useState} from 'react';
+import {QueryKeys} from '@infra';
+import {useQuery} from '@tanstack/react-query';
 
 import {userService} from '../userService';
-import {User} from '../userTypes';
 
 export function useUserGetById(id: number) {
-  const [user, setUser] = useState<User>();
-  const [error, setError] = useState<boolean | null>();
-  const [loading, setLoading] = useState(false);
-
-  const getUserById = useCallback(async () => {
-    setLoading(true);
-    try {
-      const _user = await userService.getById(id);
-      setUser(_user);
-    } catch (e) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  }, [id]);
-
-  useEffect(() => {
-    getUserById();
-  }, [getUserById]);
-
+  const {data, isError, isLoading} = useQuery({
+    queryKey: [QueryKeys.UserGetById, id],
+    queryFn: () => userService.getById(id),
+  });
   return {
-    user,
-    error,
-    loading,
+    user: data,
+    isError,
+    isLoading,
   };
 }
