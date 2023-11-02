66434c58012cc0e6bac8a8d2d1d288ca80caff03
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-11-02 13:47:11 -0400

    - The test file contains a test case for the formatRelative function that checks if the function correctly displays the time in seconds if it is less than 1 minute ago.

---
+++ src/utils/__tests__/dateUtils.spec.ts
@@ -0,0 +1,23 @@
+import {dateUtils} from '@utils';
+import {sub, formatISO} from 'date-fns';
+
+const MOCKED_NOW = 1600000000000;
+
+describe('dateUtils', () => {
+  describe('formatRelative', () => {
+    beforeAll(() => {
+      jest.spyOn(Date, 'now').mockImplementation(() => MOCKED_NOW);
+    });
+
+    afterAll(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should be displayed in seconds if less than 1 minute ago', () => {
+      const time = sub(Date.now(), {seconds: 30});
+      const timeISO = formatISO(time);
+
+      expect(dateUtils.formatRelative(timeISO)).toBe('30 s');
+    });
+  });
+});
