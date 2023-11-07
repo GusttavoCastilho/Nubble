77409d2aadb14c72fb4509fb82dd439c8a325506
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-11-07 10:08:48 -0400

    [object Promise] Fix mocking of react-native-safe-area-context in tests

--- src/test/jest-setup.ts
+++ src/test/jest-setup.ts
@@ -1,3 +1,11 @@
+//@ts-ignore
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+
+jest.mock('react-native-safe-area-context', () => ({
+  ...mockSafeAreaContext,
+  useSafeAreaInsets: jest.fn(mockSafeAreaContext.useAppSafeAreaInsets),
+}));
+
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
   return {
