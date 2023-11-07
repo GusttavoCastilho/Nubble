c92538e06815cb8e64810d40be3f569a67593484
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-11-07 10:10:09 -0400

    [FIX] Update QueryClient options in test-utils.tsx

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,30 +2,47 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import {RenderOptions, render, renderHook} from '@testing-library/react-native';
 
 import {theme} from '@theme';
 
+const queryClientConfig: QueryClientConfig = {
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // @ts-ignore
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+};
+
 export const wrapperAllProviders = () => {
-  const queryClient = new QueryClient({
-    logger: {
-      log: console.log,
-      warn: console.warn,
-      // @ts-ignore
-      error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
-    },
-    defaultOptions: {
-      queries: {
-        retry: false,
-        cacheTime: Infinity,
-      },
-      mutations: {
-        retry: false,
-        cacheTime: Infinity,
-      },
-    },
-  });
+  const queryClient = new QueryClient(queryClientConfig);
+  return ({children}: {children: React.ReactNode}) => (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export const wrapperScreenProviders = () => {
+  const queryClient = new QueryClient(queryClientConfig);
   return ({children}: {children: React.ReactNode}) => (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
@@ -42,6 +59,13 @@ function customRender<T = unknown>(
   return render(ui, {wrapper: wrapperAllProviders(), ...options});
 }
 
+function renderScreen<T = unknown>(
+  ui: React.ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) {
+  return render(ui, {wrapper: wrapperScreenProviders(), ...options});
+}
+
 function customRenderHook<Result, Props>(
   renderCallback: (props: Props) => Result,
   options?: Omit<RenderOptions, 'wrapper'>,
@@ -54,4 +78,5 @@ function customRenderHook<Result, Props>(
 
 export * from '@testing-library/react-native';
 export {customRender as render};
+export {renderScreen};
 export {customRenderHook as renderHook};
