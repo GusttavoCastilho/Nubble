d6b0c0ccc17f9c9935513c4839bba4ea0fedd3ec
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-11-18 11:22:21 -0400

    test: Add unit test for PostHeader component

--- src/components/PostItem/components/__tests__/mockedData/mockedPost.ts
+++ src/components/PostItem/components/__tests__/mockedData/mockedPost.ts
@@ -1,5 +1,12 @@
 import {Post} from '@domain';
 
+export const mockedAuthor = {
+  id: 2,
+  name: 'Maria Julia',
+  profileURL: 'https://example.com',
+  userName: 'mariajulia',
+};
+
 export const mockedPost: Post = {
   id: 1,
   imageURL: 'fake-url',
@@ -7,10 +14,5 @@ export const mockedPost: Post = {
   favoriteCount: 2,
   reactionCount: 8,
   text: 'this is the text (post description)',
-  author: {
-    id: 2,
-    name: 'Maria Julia',
-    profileURL: 'https://example.com',
-    userName: 'mariajulia',
-  },
+  author: mockedAuthor,
 };
