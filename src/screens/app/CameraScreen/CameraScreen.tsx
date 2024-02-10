9ae663978157ed666158dbb84f05531a4f31db39
GusttavoCastilho <gustavocvalenciano@gmail.com>
2024-02-10 12:43:34 -0400

    feat(mock): Add mock for react-native-vision-camera module

--- src/screens/app/CameraScreen/CameraScreen.tsx
+++ src/screens/app/CameraScreen/CameraScreen.tsx
@@ -3,7 +3,9 @@ import React, {useState} from 'react';
 import {Box, Icon, PermissionManager} from '@components';
 import {AppScreenProps} from '@routes';
 import {Dimensions, StyleSheet} from 'react-native';
-import {useAppSafeArea} from '@hooks';
+import {useAppSafeArea, useAppState} from '@hooks';
+import {Camera, useCameraDevice} from 'react-native-vision-camera';
+import { useIsFocused } from '@react-navigation/native';
 
 const CAMERA_VIEW = Dimensions.get('screen').width;
 const CONTROL_HEIGHT = (Dimensions.get('screen').height - CAMERA_VIEW) / 2;
@@ -12,6 +14,11 @@ const CONTROL_DIFF = 30;
 export function CameraScreen({navigation}: AppScreenProps<'CameraScreen'>) {
   const {top} = useAppSafeArea();
   const [flash, setFlash] = useState(false);
+  const device = useCameraDevice('back');
+
+  const isFocused = useIsFocused();
+  const appState = useAppState();
+  const isActive = isFocused && appState === 'active';
 
   function toggleFlash() {
     setFlash(prevState => !prevState);
@@ -21,7 +28,14 @@ export function CameraScreen({navigation}: AppScreenProps<'CameraScreen'>) {
       permissionName="camera"
       description="Permita o Nubble acessar a camera">
       <Box flex={1}>
-        <Box backgroundColor="grayWhite" style={StyleSheet.absoluteFill} />
+      
+        {device != null && (
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={isActive}
+          />
+        )}
 
         <Box flex={1} justifyContent="space-between">
           <Box
